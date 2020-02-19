a36d713e58ceecec58d6801ca88b905478c3b6b9
essential-workshop-9972 <josephjones7@gmail.com>
2020-02-19 11:05:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,17 +5,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  9 / 3 
 end
 
 def multiplication(num1, num2)
+  9*3 
 end
 
 def modulo(num1, num2)
+  15 % 4 
 end
 
 def square_root(num)
+  math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+   65 + (( 5 * 10 ) / 3 )
 end
